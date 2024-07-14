15be7599f693969bee12d69bf7b62451fb0126ba
XiaoYuChen <122342235+yichenxiaoyu@users.noreply.github.com>
2024-07-14 16:13:14 +0800

    Add files via upload

---
+++ 优化/非线性规划/fun1.m
@@ -0,0 +1,3 @@
+% 目标函数
+function f=fun1(x);
+f = sum(x.^2) + 8;
---
+++ 优化/非线性规划/main1.m
@@ -0,0 +1,3 @@
+options = optimset('largescale','off'); 
+[x,y] = fmincon('fun1', rand(3,1) , [], [], [], [], zeros(3,1), [], ... 
+'fun2', options)
